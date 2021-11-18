f9b383793eb33bdf3700243778f1985d82baa9cd
RaitoOni <41661474+IRavenI@users.noreply.github.com>
2021-11-18 18:00:46 +0800

    Adds bubble, radix, quick, insertion, merge sort

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            this.chart1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(12, 12);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "Legend1";
+            series1.MarkerSize = 10;
+            series1.Name = "BubbleSort";
+            series1.YValuesPerPoint = 2;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Legend = "Legend1";
+            series2.MarkerSize = 10;
+            series2.Name = "RadixSort";
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series3.Legend = "Legend1";
+            series3.MarkerSize = 10;
+            series3.Name = "QuickSort";
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series4.Legend = "Legend1";
+            series4.Name = "InsertionSort";
+            series5.ChartArea = "ChartArea1";
+            series5.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series5.Legend = "Legend1";
+            series5.Name = "MergeSort";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Series.Add(series3);
+            this.chart1.Series.Add(series4);
+            this.chart1.Series.Add(series5);
+            this.chart1.Size = new System.Drawing.Size(1301, 827);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1325, 851);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,365 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Diagnostics;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CreateArr(arr1, 10);
+            sw.Start();
+            BubbleSort(arr1);
+            sw.Stop();
+            chart1.Series["BubbleSort"].Points.AddXY("10", sw.ElapsedMilliseconds);
+            sw.Reset();
+
+            CreateArr(arr2, 100);
+            sw.Start();
+            BubbleSort(arr2);
+            sw.Stop();
+            chart1.Series["BubbleSort"].Points.AddXY("100", sw.ElapsedMilliseconds);
+            sw.Reset();
+
+            CreateArr(arr3, 1000);
+            sw.Start();
+            BubbleSort(arr3);
+            sw.Stop();
+            chart1.Series["BubbleSort"].Points.AddXY("1000", sw.ElapsedMilliseconds);
+            sw.Reset();
+
+            //CreateArr(arr4, 9000);
+            //sw.Start();
+            //BubbleSort(arr4);
+            //sw.Stop();
+            //chart1.Series["BubbleSort"].Points.AddXY("9000", sw.ElapsedMilliseconds);
+            //sw.Reset();
+
+
+            //1301; 827
+
+            CreateArr(arr1, 10);
+            sw.Start();
+            RadixSort(arr1);
+            sw.Stop();
+            chart1.Series["RadixSort"].Points.AddXY("10", sw.ElapsedMilliseconds);
+            sw.Reset();
+
+            CreateArr(arr2, 100);
+            sw.Start();
+            RadixSort(arr2);
+            sw.Stop();
+            chart1.Series["RadixSort"].Points.AddXY("100", sw.ElapsedMilliseconds);
+            sw.Reset();
+
+            CreateArr(arr3, 1000);
+            sw.Start();
+            RadixSort(arr3);
+            sw.Stop();
+            chart1.Series["RadixSort"].Points.AddXY("1000", sw.ElapsedMilliseconds);
+            sw.Reset();
+
+            CreateArr(arr4, 9000);
+            sw.Start();
+            RadixSort(arr4);
+            sw.Stop();
+            chart1.Series["RadixSort"].Points.AddXY("9000", sw.ElapsedMilliseconds);
+            sw.Reset();
+
+
+
+
+            CreateArr(arr1, 10);
+            sw.Start();
+            QuickSort(arr1);
+            sw.Stop();
+            chart1.Series["QuickSort"].Points.AddXY("10", sw.ElapsedMilliseconds);
+            sw.Reset();
+
+            CreateArr(arr2, 100);
+            sw.Start();
+            QuickSort(arr2);
+            sw.Stop();
+            chart1.Series["QuickSort"].Points.AddXY("100", sw.ElapsedMilliseconds);
+            sw.Reset();
+
+            CreateArr(arr3, 1000);
+            sw.Start();
+            QuickSort(arr3);
+            sw.Stop();
+            chart1.Series["QuickSort"].Points.AddXY("1000", sw.ElapsedMilliseconds);
+            sw.Reset();
+
+            CreateArr(arr4, 9000);
+            sw.Start();
+            QuickSort(arr4);
+            sw.Stop();
+            chart1.Series["QuickSort"].Points.AddXY("9000", sw.ElapsedMilliseconds);
+            sw.Reset();
+
+
+
+
+            CreateArr(arr1, 10);
+            sw.Start();
+            InsertionSort(arr1);
+            sw.Stop();
+            chart1.Series["InsertionSort"].Points.AddXY("10", sw.ElapsedMilliseconds);
+            sw.Reset();
+
+            CreateArr(arr2, 100);
+            sw.Start();
+            InsertionSort(arr2);
+            sw.Stop();
+            chart1.Series["InsertionSort"].Points.AddXY("100", sw.ElapsedMilliseconds);
+            sw.Reset();
+
+            CreateArr(arr3, 1000);
+            sw.Start();
+            InsertionSort(arr3);
+            sw.Stop();
+            chart1.Series["InsertionSort"].Points.AddXY("1000", sw.ElapsedMilliseconds);
+            sw.Reset();
+
+            //CreateArr(arr4, 9000);
+            //sw.Start();
+            //InsertionSort(arr4);
+            //sw.Stop();
+            //chart1.Series["InsertionSort"].Points.AddXY("9000", sw.ElapsedMilliseconds);
+            //sw.Reset();
+
+
+
+
+            CreateArr(arr1, 10);
+            sw.Start();
+            MergeSort(arr1);
+            sw.Stop();
+            chart1.Series["MergeSort"].Points.AddXY("10", sw.ElapsedMilliseconds);
+            sw.Reset();
+
+            CreateArr(arr2, 100);
+            sw.Start();
+            MergeSort(arr2);
+            sw.Stop();
+            chart1.Series["MergeSort"].Points.AddXY("100", sw.ElapsedMilliseconds);
+            sw.Reset();
+
+            CreateArr(arr3, 1000);
+            sw.Start();
+            MergeSort(arr3);
+            sw.Stop();
+            chart1.Series["MergeSort"].Points.AddXY("1000", sw.ElapsedMilliseconds);
+            sw.Reset();
+
+            CreateArr(arr4, 9000);
+            sw.Start();
+            MergeSort(arr4);
+            sw.Stop();
+            chart1.Series["MergeSort"].Points.AddXY("9000", sw.ElapsedMilliseconds);
+            sw.Reset();
+
+        }
+
+
+
+
+
+
+        static int[] BubbleSort(int[] arr)
+        {
+            int temp;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] > arr[j])
+                    {
+                        temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+            return arr;
+        }
+
+
+
+        static void RadixSort(int[] arr)
+        {
+            int i, j;
+            int[] tmp = new int[arr.Length];
+            for (int shift = 31; shift > -1; --shift)
+            {
+                j = 0;
+                for (i = 0; i < arr.Length; ++i)
+                {
+                    bool move = (arr[i] << shift) >= 0;
+                    if (shift == 0 ? !move : move)
+                        arr[i - j] = arr[i];
+                    else
+                        tmp[j++] = arr[i];
+                }
+                Array.Copy(tmp, 0, arr, arr.Length - j, j);
+            }
+        }
+
+
+
+
+        static void Swap(ref int x, ref int y)
+        {
+            var t = x;
+            x = y;
+            y = t;
+        }
+        static int Partition(int[] array, int minIndex, int maxIndex)
+        {
+            var pivot = minIndex - 1;
+            for (var i = minIndex; i < maxIndex; i++)
+            {
+                if (array[i] < array[maxIndex])
+                {
+                    pivot++;
+                    Swap(ref array[pivot], ref array[i]);
+                }
+            }
+
+            pivot++;
+            Swap(ref array[pivot], ref array[maxIndex]);
+            return pivot;
+        }
+        static int[] QuickSort(int[] array, int minIndex, int maxIndex)
+        {
+            if (minIndex >= maxIndex)
+            {
+                return array;
+            }
+
+            var pivotIndex = Partition(array, minIndex, maxIndex);
+            QuickSort(array, minIndex, pivotIndex - 1);
+            QuickSort(array, pivotIndex + 1, maxIndex);
+
+            return array;
+        }
+
+        static int[] QuickSort(int[] array)
+        {
+            return QuickSort(array, 0, array.Length - 1);
+        }
+
+
+        static int[] InsertionSort(int[] array)
+        {
+            for (var i = 1; i < array.Length; i++)
+            {
+                var key = array[i];
+                var j = i;
+                while ((j > 1) && (array[j - 1] > key))
+                {
+                    Swap(ref array[j - 1], ref array[j]);
+                    j--;
+                }
+
+                array[j] = key;
+            }
+
+            return array;
+        }
+
+
+        static void Merge(int[] array, int lowIndex, int middleIndex, int highIndex)
+        {
+            var left = lowIndex;
+            var right = middleIndex + 1;
+            var tempArray = new int[highIndex - lowIndex + 1];
+            var index = 0;
+
+            while ((left <= middleIndex) && (right <= highIndex))
+            {
+                if (array[left] < array[right])
+                {
+                    tempArray[index] = array[left];
+                    left++;
+                }
+                else
+                {
+                    tempArray[index] = array[right];
+                    right++;
+                }
+
+                index++;
+            }
+
+            for (var i = left; i <= middleIndex; i++)
+            {
+                tempArray[index] = array[i];
+                index++;
+            }
+
+            for (var i = right; i <= highIndex; i++)
+            {
+                tempArray[index] = array[i];
+                index++;
+            }
+
+            for (var i = 0; i < tempArray.Length; i++)
+            {
+                array[lowIndex + i] = tempArray[i];
+            }
+        }
+
+        static int[] MergeSort(int[] array, int lowIndex, int highIndex)
+        {
+            if (lowIndex < highIndex)
+            {
+                var middleIndex = (lowIndex + highIndex) / 2;
+                MergeSort(array, lowIndex, middleIndex);
+                MergeSort(array, middleIndex + 1, highIndex);
+                Merge(array, lowIndex, middleIndex, highIndex);
+            }
+
+            return array;
+        }
+
+        static int[] MergeSort(int[] array)
+        {
+            return MergeSort(array, 0, array.Length - 1);
+        }
+
+
+
+
+        static int[] CreateArr(int[] arr, int n)
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = rnd.Next();
+            }
+            return arr;
+        }
+
+        Stopwatch sw = new Stopwatch();
+        int[] arr1 = new int[10];
+        int[] arr2 = new int[100];
+        int[] arr3 = new int[1000];
+        int[] arr4 = new int[9000];
+
+    }
+}
